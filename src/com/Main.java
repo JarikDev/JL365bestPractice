bdd00a9663f0b18ed1023f1a6191e9de47e2424a
ivan4000 <sxln1@mail.ru>
2018-12-07 01:42:10 +0300

    JL365 best practice

---
+++ src/com/Main.java
@@ -0,0 +1,23 @@
+package com;
+
+public class Main {
+    static int ji =5;
+    int j=6;
+    void does(int e){
+        int r=5;
+        class Local{
+            private final  static   int q=4;
+            public int w=j;
+            int t=e;
+            int y=r;
+            void method(){}
+        }
+        Local local = new Local();
+    }
+    void does2(){
+        class Local{}
+    }
+    public static void main(String[] args)   {
+
+    }
+}
